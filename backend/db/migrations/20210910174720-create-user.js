14bd4c804f0c50c26780469ff1a4f85a49ad6264
goldeneye5671 <59634572+goldeneye5671@users.noreply.github.com>
2021-09-12 21:16:44 -0700

    Finished backend and working on frontend

---
+++ backend/db/migrations/20210910174720-create-user.js
@@ -0,0 +1,42 @@
+'use strict';
+const { Validator } = require('sequelize');
+
+module.exports = (sequelize, DataTypes) => {
+  const User = sequelize.define('User', {
+    username: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [4, 30],
+        isNotEmail(value) {
+          if (Validator.isEmail(value)) {
+            throw new Error('Cannot be an email.');
+          }
+        },
+      },
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [3, 256],
+        isEmail(value) {
+          if (Validator.isEmail(value)){
+            throw new Error('Must be an email')
+          }
+        }
+      },
+    },
+    hashedPassword: {
+      type: DataTypes.STRING.BINARY,
+      allowNull: false,
+      validate: {
+        len: [60, 60]
+      },
+    },
+  }, {});
+  User.associate = function(models) {
+    // associations can be defined here
+  };
+  return User;
+};
--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,9 +1,8 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/hello/world', function(req, res) {
-    res.cookie('XSRF-TOKEN', req.csrfToken());
-    res.send('Hello World!');
-  });
+router.post('/test', function(req, res) {
+  res.json({ requestBody: req.body });
+});
   
   module.exports = router;
