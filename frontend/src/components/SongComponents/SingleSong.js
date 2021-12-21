cdf0ffe7ff3dc937c8097e1e7a0a199ef79ef4e2
goldeneye5671 <59634572+goldeneye5671@users.noreply.github.com>
2021-12-20 16:22:55 -0800

    Changed what the song card looks like. Much improvement

--- frontend/src/components/SongComponents/SingleSong.js
+++ frontend/src/components/SongComponents/SingleSong.js
@@ -23,26 +23,32 @@ export default function SingleSong({song, userId}) {
     }
 
     return (
-        <div className={"song-container"}>
-            <div className={"header"}>
+        <div className={"main-container"}>
+            {/* <div className={"header"}>
                 <h4>{song.title}</h4>
-                <button onClick={e => {dispatch(getSongUrl(song?.songUrl))}}>Play</button>
-            </div>
-            <div className={"song-information1"}>
-                {
-                    (userId === song.userId) ? 
-                        !showForm ?
-                            (
-                                <>
-                                    <button onClick={e => setShowForm(!showForm)}>Edit</button>
-                                    <button onClick={deleteHandler}>Delete</button>
-                                </>    
-                            )
+            </div> */}
+            <div className={"song-container"}>
+                <div className={"song-info"}>
+                    <h4>{song.title}</h4>
+                    <p>Unknown Artist</p>
+                </div>
+                <div className={"song-buttons"}>
+                    <button onClick={e => {dispatch(getSongUrl(song?.songUrl))}}>Play</button>
+                    {
+                        (userId === song.userId) ? 
+                            !showForm ?
+                                (
+                                    <>
+                                        <button onClick={e => setShowForm(!showForm)}>Edit</button>
+                                        <button onClick={deleteHandler}>Delete</button>
+                                    </>    
+                                )
+                            :
+                                <SongForm isEdit={true} showForm={showForm} setShowForm={setShowForm} userId={userId} songInformation={song} />
                         :
-                            <SongForm isEdit={true} showForm={showForm} setShowForm={setShowForm} userId={userId} songInformation={song} />
-                    :
-                        null
-                }
+                            null
+                    }
+                </div>
             </div>
             <Comments className={"comments-container"}comments={song.Comments} songId={song?.id} />
         </div>
